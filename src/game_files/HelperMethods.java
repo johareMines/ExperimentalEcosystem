2b537f1d94b8f2c8700e2c6aaa067150f6bb4a7f
johare <johare@mines.edu>
2022-02-24 21:14:04 -0700

    FIXED CORRUPTION

--- src/game_files/HelperMethods.java
+++ src/game_files/HelperMethods.java
@@ -2,8 +2,16 @@ package game_files;
 
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.util.Random;
 
 public class HelperMethods {
+
+	public static final int SLIME_START_AMT = 30;
+	public static final float INVISIBLE_BOUNDARY = 30;
+	public static int screenWidth;
+	public static int screenHeight;
+	public static String[] names = {"BARN HARNIT", "EGG NAD"};
+
 
 	public static GraphicsDevice getSizeOfMonitor(int screen) {
 		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
@@ -16,6 +24,28 @@ public class HelperMethods {
 		} else {
 			throw new RuntimeException ("No screens found, the fuck is u doin");
 		}
+		
 
 	}
+
+	
+	public static Random rand() {
+		return new Random();
+	}
+
+
+	public static float monteCarlo() {
+		while (true) {
+			float r1 = rand().nextFloat();
+		      float r2 = rand().nextFloat();
+		      if (r2>r1) {return r1;}
+		}
+	}
+
+
+	public static String getName() {
+		return names[rand().nextInt(names.length)];
+	}
+	
+	
 }
